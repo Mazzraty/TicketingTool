98d800cafe0b3064859d03d5cb8ba1d41e327a1b
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-05-04 14:49:08 +0300

    added env for api url

--- Frontend/HelpyFy/src/api/axios.js
+++ Frontend/HelpyFy/src/api/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+// 🔐 Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN:", token); // ✅ DEBUG
+console.log("API URL:", import.meta.env.VITE_API_URL);
+  console.log("TOKEN:", token); // debug
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
