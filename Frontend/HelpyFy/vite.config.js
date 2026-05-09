b1226b31c3cef1557660326ec64b0c0a150a230c
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-05-09 14:54:16 +0300

    fix: add missing pages for vercel

--- Frontend/HelpyFy/vite.config.js
+++ Frontend/HelpyFy/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@pages": path.resolve("src/pages"),
+      "@components": path.resolve("src/components"),
+      "@layouts": path.resolve("src/layouts"),
+      "@api": path.resolve("src/api"),
+      "@utils": path.resolve("src/utils"),
+    },
+  },
 });
