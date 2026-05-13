f6867ab3f0b5e88fcca5c1575bbee548391cf2ed
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-05-13 14:35:57 +0300

    Initial commit

--- Frontend/HelpyFy/src/layouts/AdminLayout.jsx
+++ Frontend/HelpyFy/src/layouts/AdminLayout.jsx
@@ -1,17 +1,26 @@
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   return (
     <div className="flex min-h-screen bg-[#f5faf5]">
 
-      {/* TOP NAV */}
-      <Navbar />
+      {/* SIDEBAR */}
+      <Sidebar />
 
-      {/* CONTENT */}
-      <main className="flex-1 p-4 bg-[#f5faf5]">
-        <Outlet />
-      </main>
+      {/* RIGHT SIDE */}
+      <div className="flex-1 flex flex-col">
+
+        {/* TOP NAV */}
+        <Navbar />
+
+        {/* CONTENT */}
+        <main className="flex-1 p-4 bg-[#f5faf5]">
+          <Outlet />
+        </main>
+
+      </div>
 
     </div>
   );
