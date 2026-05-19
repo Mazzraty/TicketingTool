60892b93c44797bb311b90a6fb67ab62c673da88
Mazzraty <m.aashik@mazzratydairy.com>
2026-05-19 09:47:15 +0300

    Update AssetStoreFiori.jsx

--- Frontend/HelpyFy/src/pages/AssetStoreFiori.jsx
+++ Frontend/HelpyFy/src/pages/AssetStoreFiori.jsx
@@ -132,11 +132,11 @@ export default function AssetStoreFiori() {
       <div className="mb-6">
 
         <h1 className="text-2xl font-bold text-gray-800">
-          SAP Fiori Asset Store
+          Asset Store
         </h1>
 
         <p className="text-sm text-gray-500 mt-1">
-          Enterprise Asset Management Dashboard
+           Asset Management Dashboard
         </p>
 
       </div>
