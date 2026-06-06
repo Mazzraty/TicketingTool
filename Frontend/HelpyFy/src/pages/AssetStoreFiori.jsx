b6cb0b94c8c5229ed8d0cb93f1ea13db33b8759e
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-06-06 08:27:23 +0300

    Initial commit

--- Frontend/HelpyFy/src/pages/AssetStoreFiori.jsx
+++ Frontend/HelpyFy/src/pages/AssetStoreFiori.jsx
@@ -164,7 +164,7 @@ export default function AssetStoreFiori() {
   };
 
   return (
-    <div className="min-h-screen bg-[#f5f7fa] p-6">
+      <div className="min-h-screen bg-[#f5f7fa] zoom-90">
       {/* HEADER */}
       <div className="mb-8">
         <h1 className="text-3xl font-bold text-gray-800">
