83619faea23b7ed326466ee49996eedf00346e94
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-07-30 10:30:51 +0300

    Initial commit

--- Frontend/HelpyFy/src/pages/AdminTicketDashboard.jsx
+++ Frontend/HelpyFy/src/pages/AdminTicketDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useCallback } from "react";
-import api from "../../api/axios";
+import api from "../api/axios";
 import toast from "react-hot-toast";
 import {
   Chart as ChartJS,
