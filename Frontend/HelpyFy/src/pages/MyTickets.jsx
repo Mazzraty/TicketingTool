1a06006e1398d9416b3e3ecc472f97e850e58b4b
MJM AASHIK <MAASHIK@ghadeerdairy.local>
2026-08-08 20:01:29 +0300

    Initial commit

--- Frontend/HelpyFy/src/pages/MyTickets.jsx
+++ Frontend/HelpyFy/src/pages/MyTickets.jsx
@@ -349,7 +349,7 @@ export default function MyTickets() {
                       Ticket #
                     </th>
                     <th className="px-6 py-4 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Ticket
+                      Details
                     </th>
                     <th className="px-6 py-4 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                       Status
